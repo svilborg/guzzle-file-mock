9234d7e0726cf75dd77e4e22fac171b86b692a49
S. Simeonov <svilborg@gmail.com>
2018-05-22 10:05:47 +0300

    Unit tests

--- tests/GuzzleFileMockTest.php
+++ tests/GuzzleFileMockTest.php
@@ -49,6 +49,36 @@ class GuzzleFileMockTest extends TestCase
             ->__toString());
     }
 
+    public function testPut()
+    {
+        $params = [
+            "name" => "Test"
+        ];
+
+        $response = $this->getClient()->put("users/1", [
+            "form_params" => $params
+        ]);
+
+        $this->assertNotEmpty($response->getStatusCode());
+        $this->assertNotEmpty($response->getBody());
+        $this->assertNotEmpty($response->getHeaders());
+
+        $this->assertContains("Test", $response->getBody()
+            ->__toString());
+    }
+
+    public function testDelete()
+    {
+        $response = $this->getClient()->delete("users/1", []);
+
+        $this->assertNotEmpty($response->getStatusCode());
+        $this->assertNotEmpty($response->getBody());
+        $this->assertNotEmpty($response->getHeaders());
+
+        $this->assertContains("1", $response->getBody()
+            ->__toString());
+    }
+
     private function getClient()
     {
         return new GuzzleFileMock([
